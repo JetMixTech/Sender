30b674f0b4dc1df2dde28f2d903b5ce4f39ee722
Eugeniy <yarastqt@gmail.com>
2017-02-21 15:37:40 +0300

    Update validation

--- core/validate.php
+++ core/validate.php
@@ -4,31 +4,31 @@ function validate($data) {
     $errors = [];
 
     if (empty($data['companyName'])) {
-        array_push($errors, ['companyName' => 'Field cannot be blank']);
+        $errors[] = ['companyName' => 'Field cannot be blank'];
     }
 
     if (empty($data['companyMail'])) {
-        array_push($errors, ['companyMail' => 'Field cannot be blank']);
+        $errors[] = ['companyMail' => 'Field cannot be blank'];
     }
 
     if (empty($data['companySite'])) {
-        array_push($errors, ['companySite' => 'Field cannot be blank']);
+        $errors[] = ['companySite' => 'Field cannot be blank'];
     }
 
     if (empty($data['companyPhone'])) {
-        array_push($errors, ['companyPhone' => 'Field cannot be blank']);
+        $errors[] = ['companyPhone' => 'Field cannot be blank'];
     }
 
     if (empty($data['payload']['customer'])) {
-        array_push($errors, ['customer' => 'Field cannot be blank']);
+        $errors[] = ['customer' => 'Field cannot be blank'];
     }
     
     if (empty($data['payload']['phone'])) {
-        array_push($errors, ['phone' => 'Field cannot be blank']);
+        $errors[] = ['phone' => 'Field cannot be blank'];
     }
 
     if (empty($data['payload']['time'])) {
-        array_push($errors, ['time' => 'Field cannot be blank']);
+        $errors[] = ['time' => 'Field cannot be blank'];
     }
 
     return $errors;
